0b9c3827f78006caea413e7e012b6144d63b6988
canina1337 <46906916+canina1337@users.noreply.github.com>
2021-04-01 18:05:24 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,43 +1,53 @@
-const input = document.getElementById('input');
-const btn = document.getElementById('addItem');
-const content = document.getElementById('content');
+var content = document.getElementById('content');
+var input = document.getElementById('input');
+var btn = document.getElementById('addItem');
+var todos;
 
+function LStage(){
+  todos =  content.innerHTML;
+  localStorage.setItem('todos', todos);
+}
 
+content.addEventListener('click', function (evenet) {
+    if(evenet.target.tagName === "BUTTON") {
+       var div = evenet.target.parentNode;
+       div.remove();
+       LStage()
+    }
+}, false);
 
 btn.addEventListener('click', (event) => {
-  if(!input.value) {
-    return;
-  }
-  createElement(input.value);
-  input.value = "";
-})
+   if(!input.value) {
+     return;
+   }
+   newElement(input.value);
+ })
+ 
 
-
-const createElement = (value) => {
-    console.log(value);
-    const div = document.createElement('div');
+function newElement(inputValue) {
+    var div = document.createElement('div');
     div.className = 'list__item';
-
-    const text = document.createElement('div');
-    text.textContent = value;
+    var text = document.createElement('div');
+    text.textContent = inputValue;
     text.className = 'list__text';
-
-    const btnDelete = document.createElement('button');
-    btnDelete.className = 'list__btn-delete';
-    btnDelete.textContent = 'X';
-
     div.appendChild(text);
+    document.getElementById('content').appendChild(div);
+    document.getElementById('input').value = "";
+    var btnDelete = document.createElement('BUTTON');
+    var txt = document.createTextNode("X");
+    btnDelete.className = 'list__btn-delete';
+    btnDelete.appendChild(txt);
     div.appendChild(btnDelete);
-
+    LStage()
     div.addEventListener('dblclick', function() {
       text.style.background = "#c7baba";
       text.style.textDecoration = "line-through";
       div.style.opacity = 0.5;
+      toLStageLocal();
+      
     })
-    btnDelete.addEventListener('click', (event) => {
-      content.removeChild(div);
-    })
+}
 
-    content.appendChild(div);
-    
+if(localStorage.getItem('todos')) {
+   content.innerHTML = localStorage.getItem('todos');
 }
